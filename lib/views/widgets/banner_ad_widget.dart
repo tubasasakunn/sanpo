2774f0d9850c4888e05bee6d97a50232667b467b
twakaiki <twakaiki@yahoo-corp.jp>
2023-09-06 11:54:39 +0900

    広告IDの追加

--- lib/views/widgets/banner_ad_widget.dart
+++ lib/views/widgets/banner_ad_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:flutter/foundation.dart';
+import 'dart:io';
 
 class BannerAdWidget extends StatefulWidget {
   @override
@@ -13,9 +15,17 @@ class _BannerAdWidgetState extends State<BannerAdWidget> {
   @override
   void initState() {
     super.initState();
+    String adUnitId;
+    if (kReleaseMode) {
+      // リリースモードのとき
+      adUnitId = Platform.isAndroid ? 'ca-app-pub-1282226807235434/1165725161': 'ca-app-pub-1282226807235434/4198286450';   // 本番用の広告ユニットID
+    } else {
+      // デバッグモードのとき
+      adUnitId = Platform.isAndroid ? 'ca-app-pub-3940256099942544/6300978111': 'ca-app-pub-3940256099942544/2934735716';  // テスト用の広告ユニットID
+    }
 
     _bannerAd = BannerAd(
-      adUnitId: 'ca-app-pub-3940256099942544/6300978111', // テスト用ID、実際には自分の広告ユニットIDを使用してください
+      adUnitId: adUnitId, // テスト用ID、実際には自分の広告ユニットIDを使用してください
       request: AdRequest(),
       size: AdSize.banner,
       listener: BannerAdListener(
--- lib/views/widgets/interstitial_ad_manager.dart
+++ lib/views/widgets/interstitial_ad_manager.dart
@@ -1,12 +1,22 @@
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:flutter/foundation.dart';
+import 'dart:io';
 
 class InterstitialAdManager {
   InterstitialAd? _interstitialAd;
   bool _isAdLoaded = false;
 
   void interstitialAd() {
+    String adUnitId;
+    if (kReleaseMode) {
+      // リリースモードのとき
+      adUnitId = Platform.isAndroid ? 'ca-app-pub-1282226807235434/6226480157': 'ca-app-pub-1282226807235434/2341159744';   // 本番用の広告ユニットID
+    } else {
+      // デバッグモードのとき
+      adUnitId = Platform.isAndroid ? 'ca-app-pub-3940256099942544/1033173712': 'ca-app-pub-3940256099942544/4411468910';  // テスト用の広告ユニットID
+    }
     InterstitialAd.load(
-      adUnitId: 'ca-app-pub-3940256099942544/1033173712', // テスト用ID
+      adUnitId: adUnitId, // テスト用ID
       request: AdRequest(),
       adLoadCallback: InterstitialAdLoadCallback(
         onAdLoaded: (ad) {
